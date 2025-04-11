b82ffa16438311665cf0cc7f58bacb0b529b4d1f
fractalcounty <chip@fractalcounty.com>
2025-04-10 21:57:32 -0500

    fix(brews): fix formatting and style in brews function output

--- config/fish/functions/brews.fish
+++ config/fish/functions/brews.fish
@@ -1,16 +1,24 @@
 function brews --description "Show brewed formulae and casks"
+    # argument parsing
+    set -l options (fish_opt --short=r --long=raw)
+    argparse $options -- $argv
+    or return 1 # handle parsing errors
+
     # Create a temporary file to store the output
     set -l temp_file (mktemp)
 
     # Header for Formulae
     echo (gum style --foreground "$theme_purple" --bold "Formulae:") >>$temp_file
 
-    # Format and display formulae
-    brew leaves | xargs brew deps --installed --for-each | while read -l line
+    # Format and display formulae, suppressing name clash warnings
+    # and adding brackets around dependencies
+    brew leaves | xargs brew deps --formula --installed --for-each | while read -l line
         set -l parts (string split ': ' $line)
         set -l name $parts[1]
         set -l deps $parts[2]
-        echo (gum style "• $name") (gum style --foreground "$theme_dark_gray" $deps) >>$temp_file
+        #* add brackets and style dependencies
+        set -l styled_deps (gum style --foreground "$theme_dark_gray" "[$deps]")
+        echo (gum style "• $name") $styled_deps >>$temp_file
     end
 
     echo >>$temp_file
@@ -24,8 +32,12 @@ function brews --description "Show brewed formulae and casks"
         echo (gum style "$line") >>$temp_file
     end
 
-    # Display the output using gum pager
-    gum pager --show-line-numbers=false --foreground="$theme_dark_gray" -th minimal <$temp_file
+    # Display the output using gum pager or raw cat
+    if set -q _flag_raw
+        cat $temp_file
+    else
+        gum pager --show-line-numbers=false --foreground="$theme_dark_gray" -th minimal <$temp_file
+    end
 
     # Clean up the temporary file
     rm $temp_file
